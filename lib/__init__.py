91fcf2b010c6e28fe52841db578dde63fe3e9665
Adam Ginsburg <adam@adam-macbook.local>
2009-12-09 21:15:41 -0700

    fixed comment issue (if no comments, crashed!)

--- lib/__init__.py
+++ lib/__init__.py
@@ -118,9 +118,12 @@ class ShapeList(list):
 
         for shape in self:
             text_coordlist = ["%f" % f for f in shape.coord_list]
-            print >>outf, \
-                shape.name + "(" + ",".join(text_coordlist) + ") # " + shape.comment
+            outline = shape.name + "(" + ",".join(text_coordlist) + ") " 
+            if shape.comment:
+                outline += " # " + shape.comment
 
+            print >>outf, outline
+                
         outf.close()
 
 
