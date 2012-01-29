834516963e8f3a0dc05045c5dd64c5eb564613b1
Jae-Joon Lee <lee.j.joon@gmail.com>
2012-01-30 08:02:06 +0900

    collects tests from instsalled packages.

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import pytest
 
-lib_files_with_test = ["lib/ds9_region_parser.py",
-                       "lib/region_numbers.py",
-                       "lib/ds9_attr_parser.py",
-                       "lib/parser_helper.py"]
+import pyregion
+import os
 
-pytest.main(lib_files_with_test + ['tests'])
+basedir = os.path.dirname(pyregion.__file__)
+
+lib_files_with_test = ["ds9_region_parser.py",
+                       "region_numbers.py",
+                       "ds9_attr_parser.py",
+                       "parser_helper.py"]
+
+pytest.main([os.path.join(basedir, f) for f in lib_files_with_test] \
+            + ['tests'])
