918fcb03802ab3e1bc3bb996728e1b14e117f2f6
Jae-Joon Lee <lee.j.joon@gmail.com>
2017-05-18 17:05:40 -0400

    add assert_allclose for test_cube.py

--- pyregion/tests/test_cube.py
+++ pyregion/tests/test_cube.py
@@ -2,6 +2,7 @@ import os
 from os.path import join
 from astropy.io.fits import Header
 from .. import parse
+from numpy.testing import assert_allclose
 
 rootdir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
@@ -15,4 +16,6 @@ def test_cube():
     header = demo_header()
 
     region_string = 'circle(12:04:15.065,+18:26:51.00,173.029")'
-    parse(region_string).as_imagecoord(header)
+    r = parse(region_string).as_imagecoord(header)
+
+    assert_allclose(r[0].coord_list, [117, 132, 34.6], atol=0.01)
